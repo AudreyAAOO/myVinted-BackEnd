fd336df01edd683010dd5e6dd18a802d2cc1e52a
AudreyAAOO <audrey.s.carpentier@gmail.com>
2023-02-13 00:18:35 +0100

    corr Array ds Offer

--- models/Offer.js
+++ models/Offer.js
@@ -6,26 +6,9 @@ const Offer = mongoose.model("Offer", {
 	product_name: { type: String, required: true, maxLength: 50 },
 	product_description: { type: String, required: true, maxLength: 500 },
 	product_price: { type: Number, min: 0, max: 100000 },
-
-	product_details: {
-		product_brand: String,
-		product_size: String,
-		product_color: String,
-		product_condition: {
-			type: String,
-			enum: [
-				"neuf avec étiquette",
-				"neuf sans étiquette",
-				"comme neuf",
-				"occasion",
-				"légèrement abimé",
-				"médiocre",
-			],
-		},
-	},
-	// manque emplacement
+	product_details: Array,
 	product_city: String,
-	product_image: Object,
+	product_image: { type: mongoose.Schema.Types.Mixed, default: {} },
 	product_pictures: Array,
 	owner: {
 		type: mongoose.Schema.Types.ObjectId,
