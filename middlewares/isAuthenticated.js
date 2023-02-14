4f1358dd2e876408f5cd613873c6c930f5502b48
AudreyAAOO <audrey.s.carpentier@gmail.com>
2023-02-14 08:36:35 +0100

    ajout consolelog

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -8,15 +8,19 @@ const isAuthenticated = async (req, res, next) => {
 			const user = await User.findOne({ // Chercher dans la BDD un user qui a ce token
 				token: req.headers.authorization.replace("Bearer ", ""),
 			}).select("account _id");
-
+			console.log("if différent de user");
 			if (!user) {
+				console.log("if req.headers.authorization");
 				return res.status(401).json({ error: "⛔ Unauthorized" });
+
 			} else {
 				req.user = user;
+				console.log("else user");
 				// On crée une clé "user" dans req. La route dans laquelle le middleware est appelé pourra avoir accès à req.user
 				return next();
 			}
 		} else {
+			console.log("else req.headers.authorization");
 			return res.status(401).json({ error: "Unauthorized ⛔" });
 		}
 	} catch (error) {
