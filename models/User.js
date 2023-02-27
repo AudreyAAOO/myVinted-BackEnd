41b470015b876724411c936678da4a2cc4237ced
AudreyAAOO <audrey.s.carpentier@gmail.com>
2023-02-27 13:05:29 +0100

    retrait modif User

--- models/User.js
+++ models/User.js
@@ -6,7 +6,8 @@ const User = mongoose.model("User", {
 	email: { type: String, unique: true }, // `email` must be unique
 	account: {
 		username: { type: String, required: true },
-		avatar: { type: mongoose.Schema.Types.Mixed, default: {} },
+		// avatar: { type: mongoose.Schema.Types.Mixed, default: {} },
+		avatar: Object,
 	},
 	newsletter: { type: Boolean, default: false },
 	token: String,
