f5fd53e57cf7c6212e3a4a3f758ad4d5d25c77d3
AudreyAAOO <audrey.s.carpentier@gmail.com>
2023-07-13 01:08:19 +0200

    ✨ maj payment

--- models/Transaction.js
+++ models/Transaction.js
@@ -7,8 +7,8 @@ const Transaction = mongoose.model("Transaction", {
     amount: { type: Number, min: 0, max: 100000 },
     currency: String,
     title: { type: String, required: true, maxLength: 500 },
-    token: String, 
-    date_of_payment: { type: Date, default: Date.now },
+    source: { type: Object },
+    //date_of_payment: { type: Date, default: Date.now },
     offer: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Offer",
--- routes/payment.js
+++ routes/payment.js
@@ -12,15 +12,16 @@ router.post("/payment", async (req, res) => {
 
     try {
         console.log("req.body: ", req.body);
-        const stripeToken = req.body.stripeToken;// Réception du token créer via l'API Stripe depuis le front
-
+        const stripeToken = req.body.source;// Réception du token créer via l'API Stripe depuis le front
+        console.log("stripeToken:", stripeToken);
         // Créer une requête à stripe pour créer une transaction
         const payment = await stripe.charges.create({
             amount: req.body.amount,
-            currency: req.body.currency,
-            title: req.body.title,
-            token: stripeToken,  // On envoie ici le token
-            date_of_payment: new Date(date_of_payment)
+            currency: "eur",
+            //title: req.body.title,
+            description: `Paiement vinted pour : ${req.body.title}`,
+            source: req.body.source,  // On envoie ici le token
+            //date_of_payment: req.body.date_of_payment
         });
 
         console.log("response.status: ", response.status);
@@ -29,25 +30,28 @@ router.post("/payment", async (req, res) => {
         // Si le paiement est effectué, on met à jour l'offre et on renvoie au front le fait que tout s'est bien passé
         console.log("responseFromStripe- payment :", payment);
 
- // Crée une transaction grâce au model
- const newTransaction = new Transaction({ amount, currency, title, token, date_of_payment  });
+        // Crée une transaction grâce au model
+        const newTransaction = new Transaction({ amount, currency, title, description, token, source }); // date_of_payment
 
         // Sauvegarder la transaction dans une BDD MongoDB
-        await newTransaction.save();
         const response = {
             // _id: newTransaction._id,
             amount: newTransaction.amount,
             currency: newTransaction.currency,
             title: newTransaction.title,
-            token: newTransaction.stripeToken,
-            date_of_payment: { type: Date, default: Date.now },
+            description: newTransaction.description,
+            // token: newTransaction.token,
+            source: newTransaction.source,
+            // date_of_payment: newTransaction.date_of_payment,
         };
+
+        console.log('response', response);
+        await newTransaction.save(response);
+
 
         // Je renvoie au client le status de la réponse de stripe
         res.json(newTransaction.status);
         console.log("newTransaction.status", newTransaction.status);
-        res.json(response);
-        console.log("response", response);
     } catch (error) {
         console.log("error.message: ", error.message);
         res.status(400).json({ message: error.message });
